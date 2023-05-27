09b643a31b992820421e2d1ec68fd7999e538920
kanogame <wasdqert1222@gmail.com>
2023-05-27 19:23:45 +0700

    added inline compiler

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #/!bin/bash
 
+if [ "$1" != "" ] && [ "$2" != "" ]; then 
+	g++ ./$1/$2 -o result
+	return
+fi
 
 echo "write folder to compile:"
 read foldername
-echo "write name of file"
+echo "write name of file:"
 read filename
 
 g++ ./$foldername/$filename -o result
