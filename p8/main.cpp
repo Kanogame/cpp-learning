eeaf7fbee5622d1f57018b00981e83cda36bd3b8
kanogame <wasdqert1222@gmail.com>
2023-07-03 22:19:53 +0700

    ex 1

--- p8/main.cpp
+++ p8/main.cpp
@@ -2,13 +2,28 @@
 
 void print(std::string, const unsigned = 18);
 
-int main() {
-    print("Sam");       // Sam 18
-    print("Tom", 22);   // Tom 22
-    return 0
+std::string reverse(std::string input) {
+    const int length = input.length();
+    std::string res;
+
+    for (int i = 0; i < length; i++) {
+        res += input[input.length() - 1 - i];
+    }
+
+    return res;
 }
 
+
+int main() {
+    //print("Sam");       // Sam 18
+    //print("Tom", 22);   // Tom 22
+    std::string arr;
+    std::cin >> arr;
+    std::cout << reverse(arr) << std::endl;
+    return 0;
+}
+/*
 void print(std::string name, const unsigned age) {
     //age++; - нельзя
     std::cout << name << age << std::endl;
-}
+}*/
